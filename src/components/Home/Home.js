fbb2b8cc6153ffad47e8923b4e58b6ebeec0d3ea
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 <it-jg@o2.pl>
2024-02-19 10:06:45 +0100

    init

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomeHeader from './HomeHeader';
+import HomeThreeColumns from './HomeThreeColumns';
+
+const Home = () => {
+  return (
+    <div>
+      <HomeHeader />
+      <HomeThreeColumns />
+    </div>
+  );
+}
+
+export default Home;
